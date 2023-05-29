523c7cd9112b0f8ec6869af640b3fd0be6b96fbc
Haris <muhamad_haris@hotmail.com>
2023-05-29 21:11:39 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function Experience() {
+  return (
+    <>
+      <h3>Software Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>Haris Samingan</h1>
+      <h2>Software developer</h2>
+      <Experience />
+    </>
+  );
+}
+
+export default App;
